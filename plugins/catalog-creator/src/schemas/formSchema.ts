5aaa7a3fc8aa6f9dae7b4a97a53f756262e8fb23
Aleksander Obrestad <aleksander@bekk-mac-3480.local>
2025-09-19 11:53:04 +0200

    Validate special characters

--- plugins/catalog-creator/src/schemas/formSchema.ts
+++ plugins/catalog-creator/src/schemas/formSchema.ts
@@ -1,7 +1,8 @@
 import * as z from "zod";
 
 export const formSchema = z.object({
-    name: z.string().trim().min(1, "Add a name").refine(s => !s.includes(' '), { message: "Name cannot contain space"}),
+    name: z.string().trim().min(1, "Add a name").refine(s => !s.includes(' '), { message: "Name cannot contain space"}).refine(s => !/[.!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]$/.test(s), 
+    'Name cannot end with special characters'),
     owner: z.string().trim().min(1, "Add an owner").refine(s => !s.includes(' '), { message: "Owner cannot contain space"}),
     lifecycle: z.enum(["development", "production", "deprecated"], { error: "Choose a lifecycle"}),
     type: z.enum(["service", "website", "library"], { error: "Choose a type"}),

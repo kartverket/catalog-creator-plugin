1321aac72d49124777d3ed05126f5650bbba8eed
Erik Torvbråten <erik.torvbraten@bekk.no>
2025-10-01 09:45:55 +0200

    legg til kort for hver entitet i skjemaet

--- plugins/catalog-creator/src/components/CatalogForm/CatalogForm.tsx
+++ plugins/catalog-creator/src/components/CatalogForm/CatalogForm.tsx
@@ -1,4 +1,12 @@
-import { Button, Box, Flex, Select, TextField, Icon } from '@backstage/ui';
+import {
+  Button,
+  Box,
+  Flex,
+  Select,
+  TextField,
+  Icon,
+  Card,
+} from '@backstage/ui';
 
 import {
   AllowedEntityKinds,
@@ -10,7 +18,6 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Controller, useFieldArray, useForm } from 'react-hook-form';
 import { z } from 'zod/v4';
 import { formSchema } from '../../schemas/formSchema';
-import Divider from '@mui/material/Divider';
 import { useState } from 'react';
 
 export type CatalogFormProps = {
@@ -74,152 +81,159 @@ export const CatalogForm = ({ onSubmit, currentYaml }: CatalogFormProps) => {
           <h2>Catalog-info.yaml Form</h2>
           {fields.map((entity, index) => {
             return (
-              <Flex direction="column" justify="start" key={entity.id}>
-                {index !== 0 && <Divider variant="middle" sx={{ mt: 3 }} />}
-                <Flex justify="between">
-                  <div>
-                    <Controller
-                      name={`entities.${index}.kind`}
-                      control={control}
-                      render={({ field: { onChange, onBlur, value } }) => (
-                        <Select
-                          name="kind"
-                          label="Entity kind"
-                          onBlur={onBlur}
-                          onSelectionChange={onChange}
-                          selectedKey={value}
-                          options={Object.values(AllowedEntityKinds).map(
-                            entityKind => ({
-                              value: entityKind as string,
-                              label: entityKind,
-                            }),
-                          )}
-                          isRequired
-                          isDisabled={index === 0}
-                        />
-                      )}
-                    />
-                    {errors.entities?.[index]?.kind && (
-                      <span style={{ color: 'red', fontSize: '0.75rem' }}>
-                        {errors.entities?.[index]?.kind.message}
-                      </span>
-                    )}
-                  </div>
-                  {index !== 0 && (
-                    <Button
-                      style={{ width: '40px', alignSelf: 'flex-end' }}
-                      onClick={() => remove(index)}
-                    >
-                      <Icon name="trash" />
-                    </Button>
-                  )}
-                </Flex>
-                <div>
-                  <Controller
-                    name={`entities.${index}.name`}
-                    control={control}
-                    render={({ field }) => (
-                      <TextField
-                        {...field}
-                        name="Name"
-                        label="Entity name"
-                        isRequired
+              <Card
+                style={{
+                  marginRight: '1rem',
+                  marginTop: '1rem',
+                  padding: '1rem',
+                }}
+              >
+                <Flex direction="column" justify="start" key={entity.id}>
+                  <Flex justify="between">
+                    <div>
+                      <Controller
+                        name={`entities.${index}.kind`}
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                          <Select
+                            name="kind"
+                            label="Entity kind"
+                            onBlur={onBlur}
+                            onSelectionChange={onChange}
+                            selectedKey={value}
+                            options={Object.values(AllowedEntityKinds).map(
+                              entityKind => ({
+                                value: entityKind as string,
+                                label: entityKind,
+                              }),
+                            )}
+                            isRequired
+                            isDisabled={index === 0}
+                          />
+                        )}
                       />
-                    )}
-                  />
-                  {errors.entities?.[index]?.name && (
-                    <span style={{ color: 'red', fontSize: '0.75rem' }}>
-                      {errors.entities?.[index]?.name.message}
-                    </span>
-                  )}
-                </div>
-                <div>
-                  <Controller
-                    name={`entities.${index}.owner`}
-                    control={control}
-                    render={({ field }) => (
-                      <TextField
-                        {...field}
-                        name="Owner"
-                        label="Entity owner"
-                        isRequired
-                      />
-                    )}
-                  />
-                  {errors.entities?.[index]?.owner && (
-                    <span style={{ color: 'red', fontSize: '0.75rem' }}>
-                      {errors.entities?.[index]?.owner.message}
-                    </span>
-                  )}
-                </div>
-
-                <Flex>
-                  <div>
-                    <Controller
-                      name={`entities.${index}.lifecycle`}
-                      control={control}
-                      render={({ field: { onChange, onBlur, value } }) => (
-                        <Select
-                          name="lifecycle"
-                          label="Entity lifecycle"
-                          onBlur={onBlur}
-                          onSelectionChange={onChange}
-                          selectedKey={value}
-                          options={Object.values(AllowedLifecycleStages).map(
-                            lifecycleStage => ({
-                              value: lifecycleStage as string,
-                              label: lifecycleStage,
-                            }),
-                          )}
-                          isRequired
-                        />
+                      {errors.entities?.[index]?.kind && (
+                        <span style={{ color: 'red', fontSize: '0.75rem' }}>
+                          {errors.entities?.[index]?.kind.message}
+                        </span>
                       )}
-                    />
-                    {errors.entities?.[index]?.lifecycle && (
-                      <span style={{ color: 'red', fontSize: '0.75rem' }}>
-                        {errors.entities?.[index]?.lifecycle.message}
-                      </span>
+                    </div>
+                    {index !== 0 && (
+                      <Button
+                        style={{ width: '40px', alignSelf: 'flex-end' }}
+                        onClick={() => remove(index)}
+                      >
+                        <Icon name="trash" />
+                      </Button>
                     )}
-                  </div>
-
+                  </Flex>
                   <div>
                     <Controller
-                      name={`entities.${index}.entityType`}
+                      name={`entities.${index}.name`}
                       control={control}
                       render={({ field }) => (
                         <TextField
                           {...field}
-                          name="Entity type"
-                          label="Entity type"
+                          name="Name"
+                          label="Entity name"
+                          isRequired
                         />
                       )}
                     />
-                    {errors.entities?.[index]?.entityType && (
+                    {errors.entities?.[index]?.name && (
                       <span style={{ color: 'red', fontSize: '0.75rem' }}>
-                        {errors.entities?.[index]?.entityType.message}
+                        {errors.entities?.[index]?.name.message}
+                      </span>
+                    )}
+                  </div>
+                  <div>
+                    <Controller
+                      name={`entities.${index}.owner`}
+                      control={control}
+                      render={({ field }) => (
+                        <TextField
+                          {...field}
+                          name="Owner"
+                          label="Entity owner"
+                          isRequired
+                        />
+                      )}
+                    />
+                    {errors.entities?.[index]?.owner && (
+                      <span style={{ color: 'red', fontSize: '0.75rem' }}>
+                        {errors.entities?.[index]?.owner.message}
+                      </span>
+                    )}
+                  </div>
+
+                  <Flex>
+                    <div>
+                      <Controller
+                        name={`entities.${index}.lifecycle`}
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                          <Select
+                            name="lifecycle"
+                            label="Entity lifecycle"
+                            onBlur={onBlur}
+                            onSelectionChange={onChange}
+                            selectedKey={value}
+                            options={Object.values(AllowedLifecycleStages).map(
+                              lifecycleStage => ({
+                                value: lifecycleStage as string,
+                                label: lifecycleStage,
+                              }),
+                            )}
+                            isRequired
+                          />
+                        )}
+                      />
+                      {errors.entities?.[index]?.lifecycle && (
+                        <span style={{ color: 'red', fontSize: '0.75rem' }}>
+                          {errors.entities?.[index]?.lifecycle.message}
+                        </span>
+                      )}
+                    </div>
+
+                    <div>
+                      <Controller
+                        name={`entities.${index}.entityType`}
+                        control={control}
+                        render={({ field }) => (
+                          <TextField
+                            {...field}
+                            name="Entity type"
+                            label="Entity type"
+                          />
+                        )}
+                      />
+                      {errors.entities?.[index]?.entityType && (
+                        <span style={{ color: 'red', fontSize: '0.75rem' }}>
+                          {errors.entities?.[index]?.entityType.message}
+                        </span>
+                      )}
+                    </div>
+                  </Flex>
+                  <div>
+                    <Controller
+                      name={`entities.${index}.system`}
+                      control={control}
+                      render={({ field }) => (
+                        <TextField
+                          {...field}
+                          name="System"
+                          label="Entity System"
+                        />
+                      )}
+                    />
+                    {errors.entities?.[index]?.system && (
+                      <span style={{ color: 'red', fontSize: '0.75rem' }}>
+                        {errors.entities?.[index]?.system.message}
                       </span>
                     )}
                   </div>
                 </Flex>
-                <div>
-                  <Controller
-                    name={`entities.${index}.system`}
-                    control={control}
-                    render={({ field }) => (
-                      <TextField
-                        {...field}
-                        name="System"
-                        label="Entity System"
-                      />
-                    )}
-                  />
-                  {errors.entities?.[index]?.system && (
-                    <span style={{ color: 'red', fontSize: '0.75rem' }}>
-                      {errors.entities?.[index]?.system.message}
-                    </span>
-                  )}
-                </div>
-              </Flex>
+              </Card>
             );
           })}
           <Flex direction="row" align="center" style={{ paddingTop: '1rem' }}>

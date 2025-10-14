7c11b9426c41b48867c21f0fd8ba998cb24d4ce4
Erik Torvbråten <erik.torvbraten@bekk.no>
2025-10-14 16:30:55 +0200

    fix url on PR bug

--- plugins/catalog-creator/src/controllers/githubController.ts
+++ plugins/catalog-creator/src/controllers/githubController.ts
@@ -23,13 +23,6 @@ export class GithubController {
       },
     };
 
-    const match = url.match(
-      /github\.com\/([^\/]+)\/([^\/]+)\/(blob)\/([^\/]+)\/(.+)/,
-    );
-    const owner = match![1];
-    const repo = match![2];
-    const relative_path = match![5];
-
     const yamlStrings = catalogInfo.map(val =>
       updateYaml(initialYaml[val.id] ?? emptyRequiredYaml, val),
     );
@@ -39,6 +32,24 @@ export class GithubController {
     const OctokitPlugin = Octokit.plugin(createPullRequest);
     const token = await githubAuthApi.getAccessToken();
     const octokit = new OctokitPlugin({ auth: token });
+
+    let owner;
+    let repo;
+    let relative_path;
+
+    if (url.includes('blob')) {
+      const match = url.match(
+        /github\.com\/([^\/]+)\/([^\/]+)\/(blob|tree)\/([^\/]+)\/(.+)/,
+      );
+      owner = match![1];
+      repo = match![2];
+      relative_path = match![5];
+    } else {
+      const match = url.match(/github\.com\/([^\/]+)\/([^\/]+)(.*)/);
+      owner = match![1];
+      repo = match![2];
+      relative_path = 'catalog-info.yaml';
+    }
 
     try {
       await octokit.createPullRequest({

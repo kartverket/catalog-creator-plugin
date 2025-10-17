4f8eab9d898504ad80cfcef15ee6efcf08e90dbb
Erik Torvbråten <erik.torvbraten@bekk.no>
2025-10-17 08:26:07 +0200

    fix duplicate fields from system form and base schema

--- plugins/catalog-creator/src/schemas/formSchema.ts
+++ plugins/catalog-creator/src/schemas/formSchema.ts
@@ -101,11 +101,6 @@ export const templateSchema = baseEntitySchema.extend({
 
 export const systemSchema = baseEntitySchema.extend({
   kind: z.literal('System'),
-  owner: z
-    .string()
-    .trim()
-    .min(1, 'Add an owner')
-    .refine(s => !s.includes(' '), { message: 'Owner cannot contain space' }),
   entityType: z.optional(
     z
       .string()
